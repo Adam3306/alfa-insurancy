691b9446e792322fd2010604a4790e7cf8e4f067
adam-magyar <magyaradam33@gmail.com>
2025-07-24 11:36:52 +0200

    feat: integrate Google Tag Manager for enhanced tracking and analytics, and tidy up layout metadata formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,12 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 
 export const metadata: Metadata = {
-  title:
-    "Alfa Trust Insurance Kft.",
+  title: "Alfa Trust Insurance Kft.",
   description:
     "Ügyvezetőként személyes vagyonával felel a 2014-es Ptk. alapján hozott döntéseiért. Tudjon meg többet D&O biztosítással kapcsolatban! Kérjen ajánlatot vezető tisztségviselő felelősségbiztosításra!",
-    icons: {
-      icon: "/favicon.ico",
-    },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -20,6 +19,17 @@ export default function RootLayout({
   return (
     <html lang="hu">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NDTSSFG2');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link
           rel="preload"
           href="/fonts/Antonio-Regular.woff2"
@@ -58,6 +68,16 @@ export default function RootLayout({
         <link rel="icon" href="//favicon.ico" sizes="any" />
       </head>
       <body className="antialiased font-telegraf">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NDTSSFG2"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Navbar />
         <div className="pt-16">{children}</div>
       </body>
